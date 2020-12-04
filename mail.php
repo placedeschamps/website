7709dc61793bc2ad34cf0c5f88a54b4681c0e513
placedeschamps <74466371+placedeschamps@users.noreply.github.com>
2020-12-04 20:53:02 +0100

    maj5

--- mail.php
+++
@@ -1,7 +0,0 @@
-
-    <?php
-    $retour = mail('juliette30.d@gmail.com', 'Envoi depuis la page Contact', $_POST['message'], 'From : webmaster@monsite.fr');
-    if ($retour) {
-        echo '<p>Votre message a bien été envoyé.</p>';
-    }
-    ?>
